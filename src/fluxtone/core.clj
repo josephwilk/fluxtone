50dca7e1538056cf23bd8be595ab4db1fb8d5d5f
Joseph Wilk <joe@josephwilk.net>
2014-07-11 12:37:51 -0400

    Simple example using OSC to communicate from Overtone.

--- src/fluxtone/core.clj
+++ src/fluxtone/core.clj
@@ -1,6 +1,23 @@
-(ns fluxtone.core)
+(ns fluxtone.core
+  (:use [overtone.osc]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def PORT 6543)
+(def client (osc-client "localhost" PORT))
+
+(osc-send client "/fluxus/zzz" (float 0.2))
+
+(osc-close client)
+
+(comment "Fluxus code"
+(define value 0)
+(define (test)
+  (push)
+  (when (osc-msg "/fluxus/zzz")
+  (set! value (osc 0)))
+  (translate (vector 1 0 value))
+  (draw-cube)
+  (pop))
+
+  (osc-source "6543")
+  (every-frame (test))
+)
